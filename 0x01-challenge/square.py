713a0b6a6e30c633dc8278c76a5796e3c679793d
ChristianMpai <105034854+ChristianMpai@users.noreply.github.com>
2023-01-11 04:14:21 +0200

    Update square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,28 +1,31 @@
 #!/usr/bin/python3
+"""Class square """
 
-class square():
-    
+
+class square:
+    """ Documentation """
     width = 0
-    height = 0
 
-    
-    def __init__(self, *args, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+    def __init__(self, size=0):
+        """ Documentation """
+        self.width = size
 
     def area_of_my_square(self):
         """ Area of the square """
         return self.width * self.width
 
-    def PermiterOfMySquare(self):
-        return (self.width * 2) + (self.height * 2)
+    def perimeterOfMySquare(self):
+        """ Documentation """
+        return (self.width * 4)
 
     def __str__(self):
-        return "{}/{}".format(self.width, self.height)
+        """ Documentation """
+        return "{}/{}".format(self.width, self.width)
+
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = square(12)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.perimeterOfMySquare())
